89dc7f8cd236dc68fe29913f29e63d7f8245b1f8
Pessoal <Pessoal@BOMPMTZM9354.local>
2020-05-31 19:09:25 -0300

    Correção do Cors

--- src/main/java/com/server/demeter/config/CORSFilter.java
+++ src/main/java/com/server/demeter/config/CORSFilter.java
@@ -10,6 +10,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+
 @Component
 public class CORSFilter implements Filter {
 
